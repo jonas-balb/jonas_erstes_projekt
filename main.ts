f9cdba01c965f2ec4711a97622ac55659cb28cda
jonas-balb <jonas.balb@hotmail.com>
2025-01-01 14:20:39 +0100

    Lage des Nestes rotiert: Display oben - grün, sonst gelb oder rot

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 basic.forever(function () {
-    if (input.isGesture(Gesture.LogoUp)) {
+    if (input.isGesture(Gesture.ScreenUp)) {
         Environment.ledBrightness(AnalogPin.P1, true)
         Environment.ledBrightness(AnalogPin.P2, false)
         Environment.ledBrightness(AnalogPin.P9, false)
